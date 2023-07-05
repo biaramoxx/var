8c495d1c8f3722a08f435628fd82bf54219df598
Bianca Ramos <130471180+biaramoxx@users.noreply.github.com>
2023-07-05 20:00:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("Página carregada com sucesso!");
+
+let x=5;
+var y=12;
+
+document.write('a soma de x + y é ', x + y,'<br>' )
+
+document.write('a soma de x + y é ', x - y, '<br>' )
+
+document.write('a soma de x + y é ', x * y, '<br>' )
+
+document.write('a soma de x + y é ', x / y, '<br>' )
+
+document.write('a soma de x + y é ', x == y, '<br>' )
+
+document.write('a soma de x + y é ', x < y, '<br>' )
+
+document.write('a soma de x + y é ', x > y, '<br>' )
+
+document.write('a soma de x + y é ', x != y, '<br>' )
+
